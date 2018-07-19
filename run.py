bdc9fe991023a35f1e80f13ec1a1d02925792302
Stuart Mumford <stuart@cadair.com>
2018-07-19 22:19:34 +0100

    We don't need the folder

--- run.py
+++ run.py
@@ -1,5 +1,46 @@
 import os
-from sunpybot.webapp import app
+
+from flask import Flask
+
+from werkzeug.contrib.fixers import ProxyFix
+
+from baldrick.blueprints import github, circleci
+
+"""
+Configure the App
+"""
+
+app = Flask('sunpy-bot')
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+app.integration_id = int(os.environ['GITHUB_APP_INTEGRATION_ID'])
+app.private_key = os.environ['GITHUB_APP_PRIVATE_KEY']
+
+app.bot_username = 'sunpy-bot'
+
+app.register_blueprint(github)
+app.register_blueprint(circleci)
+
+
+"""
+Configure Plugins
+"""
+
+# Register the circleci artifact checker
+import baldrick.plugins.artifact_checker  # noqa
+import baldrick.plugins.milestone_checker  # noqa
+import baldrick.plugins.towncrier_changelog_checker  # noqa
+
+
+@app.route("/")
+def index():
+    return "Nothing to see here"
+
+
+@app.route("/installation_authorized")
+def installation_authorized():
+    return "Installation authorized"
 
 # Bind to PORT if defined, otherwise default to 5000.
 port = int(os.environ.get('PORT', 5000))
